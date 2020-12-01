49148ad6ee342c1197dc2b27ed851477c510d49b
Monica Waterhouse <mwaterhouse@estudiantec.cr>
2020-11-30 20:00:22 -0600

    Allow bombs to explode after some time

--- Bomb.py
+++ Bomb.py
@@ -3,9 +3,17 @@ class Bomb:
     Class for the bombs objects
     """
 
-    def __init__(self, position, matrix):
+    def __init__(self, position, matrix, bomb_radius):
+        self.active_time = 0
         self.position = position
         self.matrix = matrix
+        self.radius = bomb_radius
+
+    def detonate(self):
+        if self.active_time > 500:
+            return True
+        self.active_time += 1
+        return False
 
     def __str__(self):
         """
